697329f01ea41a7f5fc3ddac73f3bcfffeeb5411
Ion <ionmarijuan@gmail.com>
2025-09-18 21:09:37 +0200

    Get all tickers and set schema

--- script.py
+++ script.py
@@ -10,20 +10,31 @@ tickers = []
 
 url = f"https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit={LIMIT}&sort=ticker&apiKey={POLYGON_API_KEY}"
 
+# Call the API and get a response
 response = requests.get(url)
+# Parse the JSON response
 data = response.json()
+print(data)
+# Extract tickers from the response
 for ticker in data['results']:
     tickers.append(ticker)
 
-print(len(tickers))
-
+# Handle pagination
+# JSON last element is 'next_url' which contains the URL for the next page of results, if it exists
 while 'next_url' in data and data['next_url']:
-    time.sleep(12)  # To respect rate limits
+    print("Requesting next page...")
+    # Polygon’s RESTful APIs free tier subscriptions come with a limit of 5 API requests per minute, so we pause for 12 seconds before making the next request.
+    time.sleep(12)
+    # Make a request to the next_url
     next_url = data['next_url'] + f"&apiKey={POLYGON_API_KEY}"
     response = requests.get(next_url)
     data = response.json()
+    print(data)
     for ticker in data['results']:
         tickers.append(ticker)
-    print(len(tickers))
 
+# Print the total number of tickers retrieved
 print(len(tickers))
+
+# Set ticker schema to output to CSV file
+ticker_schema = ['ticker', 'name', 'market', 'locale', 'primary_exchange', 'type', 'active', 'currency_name', 'cik', 'composite_figi', 'share_class_figi', 'last_updated_utc']
